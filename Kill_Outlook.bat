e2bcb98ab6990e7402630e39d04cf9d3bcf0ab9a
Buddster124 <45415093+Buddster124@users.noreply.github.com>
2020-12-31 10:10:29 -0600

    Create Kill_Outlook.bat

---
+++ Kill_Outlook.bat
@@ -0,0 +1,8 @@
+::Script Created By: Anthony Budd
+::Last Edit: 12/31/2020
+::Description: This script will kill all outlook processes
+
+echo off
+echo "Killing outlook.exe"
+taskkill /IM outlook.exe
+echo "outlook.exe Killed"
